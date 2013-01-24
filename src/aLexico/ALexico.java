2e9f3363078d17edf6dbffc493d092632c03b66e
padredes@gmail.com <padredes@gmail.com@f242221c-c625-c159-cbf8-f5255b4fe71d>
2013-01-24 01:06:01 +0000

    Añadido vector en el Aléxico con todas las palabras reservadas.

--- src/aLexico/ALexico.java
+++ src/aLexico/ALexico.java
@@ -1,5 +1,49 @@
 package aLexico;
 
+import java.util.Vector;
+
 public class ALexico {
+	
+	private Vector<String> palReservadas;
+	
+	public void iniciaVecPalReservadas() {
+		palReservadas.clear();
+		
+		palReservadas.add("var");
+		palReservadas.add("const");
+		palReservadas.add("var-consts");
+		palReservadas.add("instructions");
+		palReservadas.add("program");
+		palReservadas.add("swap1");
+		palReservadas.add("swap2");
+		palReservadas.add("e");
+		palReservadas.add("E");
+		
+		//tipos
+		palReservadas.add("boolean");
+		palReservadas.add("character");
+		palReservadas.add("natural");
+		palReservadas.add("integer");
+		palReservadas.add("float");
+		
+		//Valores booleanos
+		palReservadas.add("true");
+		palReservadas.add("false");
+		
+		//Operadores booleanos
+		palReservadas.add("or");
+		palReservadas.add("and");
+		palReservadas.add("not");
+		
+		//Operadores de entrada / salida
+		palReservadas.add("in");
+		palReservadas.add("out");
+		
+		//Operadores de casting
+		palReservadas.add("char");
+		palReservadas.add("nat");
+		palReservadas.add("int");
+
+	}
 
 }
