0a055c53cd6afdb9c89c67115a9d86e22241d887
javimelot@gmail.com <javimelot@gmail.com@f242221c-c625-c159-cbf8-f5255b4fe71d>
2013-02-12 15:20:24 +0000

    Bytecode con String tipoVar

--- src/parser/ByteCode.java
+++ src/parser/ByteCode.java
@@ -4,6 +4,7 @@ public class ByteCode {
 
 	private tByteCode tipoByteC;
 	private String direccion;
+	private String tipoVar;
 
 	ByteCode(tByteCode tipo) {
 		this.tipoByteC = tipo;
@@ -13,6 +14,32 @@ public class ByteCode {
 	ByteCode(tByteCode tipo, String dir) {
 		this.tipoByteC = tipo;
 		this.direccion = dir;
+	}
+	
+	ByteCode(tByteCode tipo, String dir,String tipovar) {
+		this.tipoByteC = tipo;
+		this.direccion = dir;
+		this.tipoVar = tipovar;
+	}
+
+	public tByteCode getTipoByteC() {
+		return tipoByteC;
+	}
+
+	public void setTipoByteC(tByteCode tipoByteC) {
+		this.tipoByteC = tipoByteC;
+	}
+
+	public String getTipoVar() {
+		return tipoVar;
+	}
+
+	public void setTipoVar(String tipoVar) {
+		this.tipoVar = tipoVar;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
 	}
 
 	public String getDireccion() {
@@ -26,7 +53,7 @@ public class ByteCode {
 	@Override
 	public String toString() {
 		return "ByteCode [tipoByteC=" + tipoByteC + ", direccion=" + direccion
-				+ "]";
+				+ ", tipoVar=" + tipoVar + "]";
 	}
 
 }
