5bbe95603139549c38257af5a61307407fad5272
javimelot@gmail.com <javimelot@gmail.com@f242221c-c625-c159-cbf8-f5255b4fe71d>
2013-05-31 20:49:01 +0000

    Así sI!!!

---
+++ src/src/plg/proto/Parametros.java
@@ -0,0 +1,23 @@
+package src.plg.proto;
+
+public class Parametros {
+
+	
+	private String id;
+	private String clase;
+	private String nivel;
+	private String dir;
+	private String tipo;
+
+	public Parametros(String id, String clase, String nivel, String dir, String tipo) {
+
+		this.id = id;
+		this.clase = clase;
+		this.nivel = nivel;
+		this.dir = dir;
+		this.tipo = tipo;
+	}
+	
+	
+	
+}
