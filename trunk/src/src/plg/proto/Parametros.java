26ab6da88b9ce63a77d5fbec2fe7b0ac45e04bcb
javimelot@gmail.com <javimelot@gmail.com@f242221c-c625-c159-cbf8-f5255b4fe71d>
2013-06-01 09:51:09 +0000

    Avanzo hasta justo antes de Insts. Faltan errores y repasar de toda mi parte, así que yo me enecargo

--- trunk/src/src/plg/proto/Parametros.java
+++ trunk/src/src/plg/proto/Parametros.java
@@ -18,6 +18,59 @@ public class Parametros {
 		this.tipo = tipo;
 	}
 	
+	public Parametros(String clase, String tipo) {
+		this.clase = clase;
+		this.tipo = tipo;
+	}
+	
+	public int getTam(String tipos,String clase) {
+		
+		if ( clase.equals("PF")) return 1;
+		else return 0;
+	}
+	
+	
+	public String getTipo() {
+    return tipo;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getClase() {
+		return clase;
+	}
+
+	public void setClase(String clase) {
+		this.clase = clase;
+	}
+
+	public String getNivel() {
+		return nivel;
+	}
+
+	public void setNivel(String nivel) {
+		this.nivel = nivel;
+	}
+
+	public String getDir() {
+		return dir;
+	}
+
+	public void setDir(String dir) {
+		this.dir = dir;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
 	
 	
 }
