18ae8c9d4ea3cf59c32377a2edf56817fbc3a74c
javimelot@gmail.com <javimelot@gmail.com@f242221c-c625-c159-cbf8-f5255b4fe71d>
2013-06-07 14:24:54 +0000

    main

--- src/src/plg/proto/Main.java
+++ src/src/plg/proto/Main.java
@@ -1,10 +1,6 @@
 package src.plg.proto;
 
 import java.io.FileInputStream;
-import java.util.ArrayList;
-
-import src.plg.proto.Parser;
-import src.plg.proto.Scanner;
 
 import src.es.ucm.fdi.plg.evlib.TAtributos;
 
@@ -14,20 +10,8 @@ import java_cup.runtime.*;
 
 public class Main {
   public static void main(String[] args) throws Exception {   
-     // Parser p = new Parser(new Scanner(new FileInputStream("input.txt")),new DefaultSymbolFactory());
-      //Symbol s = p.parse();
-      //System.out.println(((TAtributos)s.value).a("cod").valor());
-  //}
-	  try {
-	      Parser p = new Parser(new Scanner(new FileInputStream("input.txt")),new DefaultSymbolFactory());
-	      Symbol symbol = p.parse();
-	      TAtributos prog = (TAtributos) symbol.value;
-	      ArrayList<String> cod = (ArrayList<String>) prog.a("testError").valor();
-	      System.out.println(cod);
-//	      TablaSimbolos ts = (TablaSimbolos)prog.a("testTs").valor();
-//	      System.out.println(ts.devuelveTabla());
-		  } catch (Error e) {
-			  System.err.print(e.getMessage());
-		  }
-	  }
+      Parser p = new Parser(new Scanner(new FileInputStream("input.txt")),new DefaultSymbolFactory());
+      Symbol Programa = p.parse();
+      System.out.println(((TAtributos)Programa.value).a("cod").valor());
+  }
 }
