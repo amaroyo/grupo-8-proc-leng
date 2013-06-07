8621547a2732db269ea6b225ebac30e827e5f827
javimelot@gmail.com <javimelot@gmail.com@f242221c-c625-c159-cbf8-f5255b4fe71d>
2013-06-07 10:26:40 +0000

    vamos

--- trunk/src/tiposNuevos/TiposTupla.java
+++ trunk/src/tiposNuevos/TiposTupla.java
@@ -8,22 +8,26 @@ import src.plg.proto.Tipo;
 public class TiposTupla extends Tipo{
 
 	private ArrayList<Tipo> campos;
+	private int tamTupla;
 	
-	public TiposTupla(String tipo, int tamano,ArrayList<Tipo> campos) {
-		super(tipo, tamano);
+	public TiposTupla(String tipo,ArrayList<Tipo> campos) {
+		super(tipo);
 		// TODO Auto-generated constructor stub
 		this.campos = campos;
 	}
 	
 	
 	public String toString() {
+		tamTupla=0;
 		String tipoTotal = "< t : " + tip + " , campos : [";
 		Iterator<Tipo> it = campos.iterator();
-		while (it.hasNext()) {			
-			tipoTotal += it.next().toString();
+		while (it.hasNext()) {
+			Tipo aux=it.next();
+			tamTupla=tamTupla+aux.gettamano();
+			tipoTotal += aux.toString();
 			if (it.hasNext()) tipoTotal += " , ";
 		}
-		tipoTotal += "] , tam : " + tamano + " >";
+		tipoTotal += "] , tam : " + tamTupla + " >";
 		return tipoTotal;
 	}
 
